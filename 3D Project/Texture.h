4dfef9674c1eba9a1db73cb69ad2a98d6054cb82
Albin Bernhardsson <catscratchesp@gmail.com>
2015-02-03 11:53:10 +0100

    Document texture classes
    Add doxygen comments to texture classes.

--- 3D Project/Texture.h
+++ 3D Project/Texture.h
@@ -4,25 +4,42 @@
 #include <gl/glew.h>
 #include <gl/GL.h>
 
-// Represents a texture.
+/// Interface for textures.
+/**
+ * Used to load textures from image files.
+ */
 class Texture {
 	public:
-		// Constructor
+		/// Constructor
 		Texture() { }
 
-		// Destructor
+		/// Destructor
 		virtual ~Texture() { }
 
-		// Get OpenGL texture ID.
+		/// Get OpenGL texture ID.
+		/**
+		 * Used when binding a texture before draw calls.
+		 * @return The OpenGL texture identifier
+		 */
 		virtual GLuint textureID() const = 0;
 
-		// Get the width of the texture.
+		/// Get the width of the texture.
+		/**
+		 * @return The width of the texture in texels
+		 */
 		virtual int width() const = 0;
 
-		// Get the height of the texture.
+		/// Get the height of the texture.
+		/**
+		 * @return The height of the texture in texels
+		 */
 		virtual int height() const = 0;
 
-		// Get image GL format based on color components.
+		/// Get image GL format based on color components.
+		/**
+		 * @param components The amount of components in the image. (1, 2, 3, or 4.)
+		 * @return One of GL_RED, GL_RG, GL_RGB or GL_RGBA
+		 */
 		static GLenum format(int components);
 };
 
