6814f17eb4f8f6cf0816caf16be97f61ea236032
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2015-02-05 19:58:33 +0100

    Added doxygen comments
    Doxygen comments added for ParticleSystem

--- 3D Project/ParticleSystem.h
+++ 3D Project/ParticleSystem.h
@@ -3,41 +3,78 @@
 #include <vector>
 #include <glm\glm.hpp>
 
-//Emitter Controller
+/// Summary
+/**
+* Creates particles
+*/
 class ParticleSystem
 {
 public:
-	//Ordered pair of values, each particle has vertex that corresponds to a property.
+	/// Summary
+	/**
+	* Describes vertex properties
+	*/
 	struct Vertex
 	{
 		glm::vec3 worldPos;
 	};
+	/// Summary
+	/**
+	* Describes particle properties
+	*/
 	struct ParticleProperty
 	{
 		glm::vec3 velocity;
 		float lifetime;
 	};
-	//Constructors
+	/// Summary
+	/**
+	* Default constructor (Not in use)
+	*/
 	ParticleSystem();
+	/// Summary
+	/**
+	* param glm::vec3 origin, int maxParticleCount, int chanceToEmit (x in 1000 chance), float maxVelocity, float maxLifeTime.
+	*/
 	ParticleSystem(glm::vec3 origin, int maxParticleCount, int chanceToEmit, float maxVelocity, float maxLifeTime);
-	//Destructor
+	/// Summary
+	/**
+	* Destructor
+	*/
 	~ParticleSystem();
 
+	/// Summary
+	/**
+	* @return How many particles currently exist.
+	*/
 	unsigned int getParticleCount();
+	/// Summary
+	/**
+	* @return Maximum amount of particles.
+	*/
 	unsigned int getMaxParticleCount();
+	/// Summary
+	/**
+	* Updates the system.
+	*/
 	void update(double time);
+	/// Summary
+	/**
+	* Returns the address to the dynamic array that holds the vertices.
+	*/
 	Vertex* getStartAddress();
 private:
 	std::vector < Vertex > vertices;
 	std::vector < ParticleProperty > particleProperties;
 	
-	//Properties of the system
+	//System properties
 	glm::vec3 particleOrigin;
 	unsigned int particleCount;
 	unsigned int maxParticleCount;
 	float maxLifeTime;
 	float maxVelocity;
 	int chanceToEmit;
+
 	//Helper functions
 	void emitParticle();
 };
