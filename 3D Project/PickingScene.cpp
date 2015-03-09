3746cdf6ae1d063fc8d5e87cc8806ea5f72d55b2
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2015-03-09 16:58:34 +0100

    Optimization
    Moved per-render-call operations out from per-object loop.

--- 3D Project/PickingScene.cpp
+++ 3D Project/PickingScene.cpp
@@ -88,53 +88,65 @@ void PickingScene::render(int width, int height) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	float closestDistance = std::numeric_limits<float>::max();
 
+	// Send the matrices to the shader (Per render call operations).
+	glm::mat4 view = player->camera()->view();
+	glm::mat4 proj = player->camera()->projection(width, height);
+	//create mouse ray
+	float x, y;
+	x = static_cast<float>(input::cursorX());
+	y = static_cast<float>(input::cursorY());
+	//Get NDC x and y
+	float vx = ((2.0f * x) / width) - 1.0f;
+	float vy = 1.0f - ((2.0f * y) / height);
+	glm::mat4 inverseView = glm::inverse(view);
+	glm::mat4 inverseProj = glm::inverse(proj);
+	//Ray in clip space
+	glm::vec4 rayClip(vx, vy, -1.0f, 0.0f);
+	//Ray in view space
+	glm::vec4 rayEye = inverseProj*rayClip;
+	rayEye = glm::vec4(rayEye.x, rayEye.y, -1.0, 0.0);
+
+	// Base image texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, diffuse->textureID());
+
+	// Normal map
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, normal->textureID());
+
+	// Specular map
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, specular->textureID());
+
+	glUniformMatrix4fv(shaderProgram->uniformLocation("viewMatrix"), 1, GL_FALSE, &view[0][0]);
+	glUniformMatrix4fv(shaderProgram->uniformLocation("projectionMatrix"), 1, GL_FALSE, &player->camera()->projection(width, height)[0][0]);
+
+	// Light information.
+	glm::vec4 lightPosition = view * glm::vec4(-5.f, 0.f, 5.f, 1.f);
+	glm::vec3 lightIntensity(1.f, 1.f, 1.f);
+	glm::vec3 diffuseKoefficient(1.f, 1.f, 1.f);
+	glUniform4fv(shaderProgram->uniformLocation("lightPosition"), 1, &lightPosition[0]);
+	glUniform3fv(shaderProgram->uniformLocation("lightIntensity"), 1, &lightIntensity[0]);
+	glUniform3fv(shaderProgram->uniformLocation("diffuseKoefficient"), 1, &diffuseKoefficient[0]);
+
 	for (int i = 0; i < numModels; i++)
 	{
 		glBindVertexArray(vertexAttributes[i]);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffers[i]);
 
-		// Base image texture
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuse->textureID());
-
-		// Normal map
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normal->textureID());
-
-		// Specular map
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, specular->textureID());
 		Geometry* currGeometry = multiGeometry[i];
 		// Model matrix, unique for each model.
 		glm::mat4 model = currGeometry->modelMatrix();
 
-		// Send the matrices to the shader.
-		glm::mat4 view = player->camera()->view();
 		glm::mat4 MV = view * model;
 		glm::mat4 N = glm::transpose(glm::inverse(MV));
 
-		//create mouse ray
-		float x, y;
-		x = static_cast<float>(input::cursorX());
-		y = static_cast<float>(input::cursorY());
-		glm::mat4 proj = player->camera()->projection(width, height);
-		//Get NDC x and y
-		float vx = ((2.0f * x) / width) - 1.0f;
-		float vy = 1.0f - ((2.0f * y) / height);
-		glm::vec4 rayOrigin(0.0f, 0.0f, 0.0f, 1.0f);
-		//transform ray into local space
-		glm::mat4 inverseModel = glm::inverse(model);
-		glm::mat4 inverseView = glm::inverse(view);
-		glm::mat4 inverseProj = glm::inverse(proj);
-		//Ray in clip space
-		glm::vec4 rayClip(vx, vy, -1.0f, 0.0f);
-		//Ray in view space
-		glm::vec4 rayEye = inverseProj*rayClip;
-		rayEye = glm::vec4(rayEye.x, rayEye.y, -1.0, 0.0);
 		//Ray in world space
+		glm::vec4 rayOrigin(0.0f, 0.0f, 0.0f, 1.0f);
 		rayOrigin = inverseView*(rayOrigin);
 		glm::vec4 rayWor = glm::vec4(inverseView*rayEye);
 		//ray in local space
+		glm::mat4 inverseModel = glm::inverse(model);
 		rayOrigin = inverseModel*(rayOrigin);
 		glm::vec3 rayMod = glm::vec3(inverseModel*rayWor);
 		rayWor = glm::normalize(rayWor);
@@ -163,20 +175,9 @@ void PickingScene::render(int width, int height) {
 		//fprintf(stderr, "Raydir: %f %f %f\n", rayWor.x, rayWor.y, rayWor.z);
 		//fprintf(stderr, "RayO: %f %f %f\n", rayOrigin.x, rayOrigin.y, rayOrigin.z);
 		//fflush(stderr);
-
+		
 		glUniformMatrix4fv(shaderProgram->uniformLocation("modelMatrix"), 1, GL_FALSE, &model[0][0]);
-		glUniformMatrix4fv(shaderProgram->uniformLocation("viewMatrix"), 1, GL_FALSE, &view[0][0]);
 		glUniformMatrix3fv(shaderProgram->uniformLocation("normalMatrix"), 1, GL_FALSE, &glm::mat3(N)[0][0]);
-		glUniformMatrix4fv(shaderProgram->uniformLocation("projectionMatrix"), 1, GL_FALSE, &player->camera()->projection(width, height)[0][0]);
-
-		// Light information.
-		glm::vec4 lightPosition = view * glm::vec4(-5.f, 0.f, 5.f, 1.f);
-		glm::vec3 lightIntensity(1.f, 1.f, 1.f);
-		glm::vec3 diffuseKoefficient(1.f, 1.f, 1.f);
-
-		glUniform4fv(shaderProgram->uniformLocation("lightPosition"), 1, &lightPosition[0]);
-		glUniform3fv(shaderProgram->uniformLocation("lightIntensity"), 1, &lightIntensity[0]);
-		glUniform3fv(shaderProgram->uniformLocation("diffuseKoefficient"), 1, &diffuseKoefficient[0]);
 
 		// Draw the triangles
 		glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, (void*)0);
