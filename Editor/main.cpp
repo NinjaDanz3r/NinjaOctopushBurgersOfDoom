22e3485cf3779a322d71d901ac52ea981f028cb6
Albin Bernhardsson <catscratchesp@gmail.com>
2015-03-10 15:38:26 +0100

    Logging in editor
    Redirect stderr to editor_log.txt in appdata folder (or equivalent on
    Linux/Mac).

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -1,9 +1,11 @@
 #include <GL/glew.h>
 #include "editor.h"
+#include <util.h>
 
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[]) {
+	freopen(util::savePath("editor_log.txt").c_str(), "a", stderr);
 	QApplication a(argc, argv);
 	Editor w;
 	w.show();
