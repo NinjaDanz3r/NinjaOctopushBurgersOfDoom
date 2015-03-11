0ff3d1f5681b292583cd755cbd142c055fdd3619
Albin Bernhardsson <catscratchesp@gmail.com>
2015-03-11 10:31:23 +0100

    Fix #18
    Fix particle system slowdown.

--- Game/ParticleSystem.cpp
+++ Game/ParticleSystem.cpp
@@ -46,7 +46,7 @@ void ParticleSystem::update(double time) {
 	emitParticle();
 
 	if (particleCount > 0)
-		glBufferData(GL_ARRAY_BUFFER, particleCount * sizeof(ParticleSystem::ParticlePosition), &this->particlePositions[0], GL_DYNAMIC_DRAW);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, particleCount * sizeof(ParticleSystem::ParticlePosition), &this->particlePositions[0]);
 }
 
 void ParticleSystem::render(int width, int height, const Camera* camera) {
@@ -64,7 +64,7 @@ void ParticleSystem::render(int width, int height, const Camera* camera) {
 	glBindTexture(GL_TEXTURE_2D, texture->textureID());
 
 	// Send the matrices to the shader.
-	glm::mat4 view =camera->view();
+	glm::mat4 view = camera->view();
 
 	glUniformMatrix4fv(shaderProgram->uniformLocation("viewMatrix"), 1, GL_FALSE, &view[0][0]);
 	glUniformMatrix4fv(shaderProgram->uniformLocation("projectionMatrix"), 1, GL_FALSE, &camera->projection(width, height)[0][0]);
@@ -77,11 +77,12 @@ void ParticleSystem::bindPointData() {
 	// Vertex buffer
 	glGenBuffers(1, &vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, maxParticleCount * sizeof(ParticleSystem::ParticlePosition), NULL, GL_DYNAMIC_DRAW);
 
 	// Define vertex data layout
 	glGenVertexArrays(1, &vertexAttribute);
 	glBindVertexArray(vertexAttribute);
-	glEnableVertexAttribArray(0); //the vertex attribute object will remember its enabled attributes
+	glEnableVertexAttribArray(0);
 
 	GLuint vertexPos = shaderProgram->attributeLocation("vertex_position");
 	glVertexAttribPointer(vertexPos, 3, GL_FLOAT, GL_FALSE, sizeof(ParticleSystem::ParticlePosition), BUFFER_OFFSET(0));
