972e564a4610af723afe8fedd87625c84bc195d7
Albin Bernhardsson <catscratchesp@gmail.com>
2015-03-13 11:17:34 +0100

    Add Editor logging setting
    Add setting to enable/disable editing in editor.

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -1,16 +1,23 @@
 #include <GL/glew.h>
 #include "editor.h"
 #include <util.h>
+#include "settings.h"
 
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[]) {
-	freopen(util::savePath("editor_log.txt").c_str(), "a", stderr);
+	settings::load(util::savePath("editor_settings.ini").c_str());
+	if (settings::logging())
+		freopen(util::savePath("editor_log.txt").c_str(), "a", stderr);
 	util::logWithTime("Editor started");
+
 	QApplication a(argc, argv);
 	Editor w;
 	w.show();
 	int result = a.exec();
+
 	util::logWithTime("Editor ended");
+	settings::save(util::savePath("editor_settings.ini").c_str());
+
 	return result;
 }
