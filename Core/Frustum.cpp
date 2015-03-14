792e3c41f105ad7d60aff004bdf78ebc84888588
Albin Bernhardsson <catscratchesp@gmail.com>
2015-03-14 16:10:05 +0100

    Frustum-AABB collision
    Frustum-AABB collision detection.

--- Core/Frustum.cpp
+++ Core/Frustum.cpp
@@ -1,39 +1,72 @@
 #include "Frustum.h"
 
-Frustum::Frustum(const glm::mat4x4& matrix) {
+Frustum::Frustum(const glm::mat4& matrix) {
 	// Left clipping plane
-	planes[0].x = matrix[4][1] + matrix[1][1];
-	planes[0].y = matrix[4][2] + matrix[1][2];
-	planes[0].z = matrix[4][3] + matrix[1][3];
-	planes[0].w = matrix[4][4] + matrix[1][4];
+	planes[0].x = matrix[0][3] + matrix[0][0];
+	planes[0].y = matrix[1][3] + matrix[1][0];
+	planes[0].z = matrix[2][3] + matrix[2][0];
+	planes[0].w = matrix[3][3] + matrix[3][0];
 
 	// Right clipping plane
-	planes[1].x = matrix[4][1] - matrix[1][1];
-	planes[1].y = matrix[4][2] - matrix[1][2];
-	planes[1].z = matrix[4][3] - matrix[1][3];
-	planes[1].w = matrix[4][4] - matrix[1][4];
+	planes[1].x = matrix[0][3] - matrix[0][0];
+	planes[1].y = matrix[1][3] - matrix[1][0];
+	planes[1].z = matrix[2][3] - matrix[2][0];
+	planes[1].w = matrix[3][3] - matrix[3][0];
 
 	// Top clipping plane
-	planes[2].x = matrix[4][1] - matrix[2][1];
-	planes[2].y = matrix[4][2] - matrix[2][2];
-	planes[2].z = matrix[4][3] - matrix[2][3];
-	planes[2].w = matrix[4][4] - matrix[2][4];
+	planes[2].x = matrix[0][3] - matrix[0][1];
+	planes[2].y = matrix[1][3] - matrix[1][1];
+	planes[2].z = matrix[2][3] - matrix[2][1];
+	planes[2].w = matrix[3][3] - matrix[3][1];
 
 	// Bottom clipping plane
-	planes[3].x = matrix[4][1] + matrix[2][1];
-	planes[3].y = matrix[4][2] + matrix[2][2];
-	planes[3].z = matrix[4][3] + matrix[2][3];
-	planes[3].w = matrix[4][4] + matrix[2][4];
+	planes[3].x = matrix[0][3] + matrix[0][1];
+	planes[3].y = matrix[1][3] + matrix[1][1];
+	planes[3].z = matrix[2][3] + matrix[2][1];
+	planes[3].w = matrix[3][3] + matrix[3][1];
 
 	// Near clipping plane
-	planes[4].x = matrix[4][1] + matrix[3][1];
-	planes[4].y = matrix[4][2] + matrix[3][2];
-	planes[4].z = matrix[4][3] + matrix[3][3];
-	planes[4].w = matrix[4][4] + matrix[3][4];
+	planes[4].x = matrix[0][3] + matrix[0][2];
+	planes[4].y = matrix[1][3] + matrix[1][2];
+	planes[4].z = matrix[2][3] + matrix[2][2];
+	planes[4].w = matrix[3][3] + matrix[3][2];
 
 	// Far clipping plane
-	planes[5].x = matrix[4][1] - matrix[3][1];
-	planes[5].y = matrix[4][2] - matrix[3][2];
-	planes[5].z = matrix[4][3] - matrix[3][3];
-	planes[5].w = matrix[4][4] - matrix[3][4];
+	planes[5].x = matrix[0][3] - matrix[0][2];
+	planes[5].y = matrix[1][3] - matrix[1][2];
+	planes[5].z = matrix[2][3] - matrix[2][2];
+	planes[5].w = matrix[3][3] - matrix[3][2];
+}
+
+bool Frustum::collide(const AABB& aabb) const {
+	// Define the AABB's vertices.
+	glm::vec3 vertices[8];
+	vertices[0] = aabb.minVertex;
+	vertices[1] = aabb.minVertex + glm::vec3(aabb.dimensions.x, 0.f, 0.f);
+	vertices[2] = aabb.minVertex + glm::vec3(0.f, aabb.dimensions.y, 0.f);
+	vertices[3] = aabb.minVertex + glm::vec3(0.f, 0.f, aabb.dimensions.z);
+	vertices[4] = aabb.maxVertex;
+	vertices[5] = aabb.maxVertex - glm::vec3(aabb.dimensions.x, 0.f, 0.f);
+	vertices[6] = aabb.maxVertex - glm::vec3(0.f, aabb.dimensions.y, 0.f);
+	vertices[7] = aabb.maxVertex - glm::vec3(0.f, 0.f, aabb.dimensions.z);
+
+	// Check if the AABB's vertices lie in the planes or in their positive halfspaces.
+	// Only one vertex has to lie inside the halfspace for each plane.
+	for (int plane = 0; plane < 6; plane++) {
+		bool inside = false;
+		for (int vertex = 0; vertex < 8; vertex++) {
+			if (distanceToPoint(planes[plane], vertices[vertex]) >= 0.f) {
+				inside = true;
+				break;
+			}
+		}
+
+		if (!inside)
+			return false;
+	}
+	return true;
+}
+
+float Frustum::distanceToPoint(const glm::vec4& plane, const glm::vec3& point) {
+	return plane.x * point.x + plane.y * point.y + plane.z * point.z + plane.w;
 }
--- Core/Frustum.h
+++ Core/Frustum.h
@@ -3,6 +3,7 @@
 
 #include "Core.h"
 #include <glm/glm.hpp>
+#include "AABB.h"
 
 /** @ingroup core
 * @{
@@ -18,10 +19,21 @@ class Frustum {
 		/**
 		 * @param matrix View-projection matrix to create frustum planes from.
 		 */
-		CORE_EXPORT Frustum(const glm::mat4x4& matrix);
+		CORE_EXPORT Frustum(const glm::mat4& matrix);
+
+		/// Check collision between frustum and an AABB.
+		/**
+		 * @param aabb The AABB to check collision against.
+		 * @return Whether there was a collision
+		 */
+		CORE_EXPORT bool collide(const AABB& aabb) const;
 
 	private:
 		glm::vec4 planes[6];
+
+		// The distance returned is the signed distance in normal vector units.
+		// To ensure the "true" distance the plane equation has to be normalized.
+		static float distanceToPoint(const glm::vec4& plane, const glm::vec3& point);
 };
 
 /** @} */
