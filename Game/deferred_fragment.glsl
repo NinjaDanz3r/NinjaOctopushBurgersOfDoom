5482ac78c0bf901634158b8a6af25f7119ae523f
splusharn <splusharn@gmail.com>
2015-03-19 01:18:13 +0100

    Implemented stratified and normal Poisson sampling
    Stratified is very expensive. Clean up code and fix lighting to not be
    hardcoded.

--- Game/deferred_fragment.glsl
+++ Game/deferred_fragment.glsl
@@ -25,7 +25,7 @@ out vec4 fragment_color;
 
 
 const float EPSILON = 0.03;
-
+//For Stratified Poisson Sampling (EXPENSIVE AND NOT THAT GREAT)
 float random(vec3 seed, int i){
 	vec4 seed4 = vec4(seed,i);
 	float dot_product = dot(seed4, vec4(12.9898,78.233,45.164,94.673));
