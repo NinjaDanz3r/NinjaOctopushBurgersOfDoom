b3056d2981b2305db6c8aeb9f84f7f78bcac1aba
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2015-03-19 20:19:49 +0100

    Added additional culling.

--- Game/FrustumScene.cpp
+++ Game/FrustumScene.cpp
@@ -139,45 +139,36 @@ void FrustumScene::render(int width, int height) {
 	glUniformMatrix4fv(shaderProgram->uniformLocation("viewMatrix"), 1, GL_FALSE, &view[0][0]);
 	glUniformMatrix4fv(shaderProgram->uniformLocation("projectionMatrix"), 1, GL_FALSE, &player->camera()->projection(width, height)[0][0]);
 	glBindVertexArray(geometry->vertexArray());
+
 	// Drawing loop
 	int objectsRendered = 0;
+	
+	//Frustum in world space
 	frustum = new Frustum(player->camera()->projection(width, height) * player->camera()->view());
 	qTree->getObjects(*frustum, geometryMap);
+	delete frustum;
+
 	typedef std::map<GeometryObject*, GeometryObject*>::iterator it_type;
 	for (it_type iterator = geometryMap.begin(); iterator != geometryMap.end(); iterator++) {
-		objectsRendered++;
+		
 		// Model matrix, unique for each model.
 		glm::mat4 model = iterator->second->modelMatrix();
+		//Frustum local to objects
+		frustum = new Frustum(player->camera()->projection(width, height) * player->camera()->view() * model);
+		if (frustum->collide(geometry->aabb))
+		{
+			glm::mat4 MV = view * model;
+			glm::mat4 N = glm::transpose(glm::inverse(MV));
 
-		glm::mat4 MV = view * model;
-		glm::mat4 N = glm::transpose(glm::inverse(MV));
+			glUniformMatrix4fv(shaderProgram->uniformLocation("modelMatrix"), 1, GL_FALSE, &model[0][0]);
+			glUniformMatrix3fv(shaderProgram->uniformLocation("normalMatrix"), 1, GL_FALSE, &glm::mat3(N)[0][0]);
 
-		glUniformMatrix4fv(shaderProgram->uniformLocation("modelMatrix"), 1, GL_FALSE, &model[0][0]);
-		glUniformMatrix3fv(shaderProgram->uniformLocation("normalMatrix"), 1, GL_FALSE, &glm::mat3(N)[0][0]);
-
-		glDrawElements(GL_TRIANGLES, geometry->indexCount(), GL_UNSIGNED_INT, (void*)0);
+			glDrawElements(GL_TRIANGLES, geometry->indexCount(), GL_UNSIGNED_INT, (void*)0);
+			objectsRendered++;
+		}
 	}
 	Game::additionalData = std::to_string(objectsRendered);
 	geometryMap.clear();
-
-	//Frustum culling against AABB's
-	//for (int i = 0; i < numModels; i++)
-	//{
-	//	glm::mat4 model = multiGeometry[i]->modelMatrix();
-	//	frustum = new Frustum(player->camera()->projection(width, height)[0][0] * player->camera()->view() * glm::inverse(model));
-	//	if (frustum->collide(geometry->aabb))
-	//	{
-	//		objectsRendered++;
-
-	//		glm::mat4 MV = view * model;
-	//		glm::mat4 N = glm::transpose(glm::inverse(MV));
-
-	//		glUniformMatrix4fv(shaderProgram->uniformLocation("modelMatrix"), 1, GL_FALSE, &model[0][0]);
-	//		glUniformMatrix3fv(shaderProgram->uniformLocation("normalMatrix"), 1, GL_FALSE, &glm::mat3(N)[0][0]);
-
-	//		glDrawElements(GL_TRIANGLES, geometry->indexCount(), GL_UNSIGNED_INT, (void*)0);
-	//	}
-	//}
 
 	if (state == 1) {
 		multipleRenderTargets->showTextures(width, height);
