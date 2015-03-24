93381d1919c4cc45aad762e0f377357dbc0486cf
Albin Bernhardsson <catscratchesp@gmail.com>
2015-03-24 14:34:09 +0100

    Clean up particle geometry shader
    Maybe slightly faster?

--- Game/particle_geometry.glsl
+++ Game/particle_geometry.glsl
@@ -25,34 +25,34 @@ out VertexData {
 } vertexOut;
 
 void main() {
-	//Up relative to camera extracted from view matrix
+	// Up relative to camera extracted from view matrix
 	vec4 cameraUp = vec4(viewMatrix[0][1], viewMatrix[1][1], viewMatrix[2][1], 0.0);
 
-	//vector from camera to point, cameras position extracted from view matrix
+	// vector from camera to point, cameras position extracted from view matrix
 	vec4 look = vec4(cameraPosition, 1.f) - vec4(gl_in[0].gl_Position.xyz, 1.0);
 
-	//Particle right vector = camera up x look
+	// Particle right vector = camera up x look
 	vec4 right = normalize(vec4(cross(cameraUp.xyz, look.xyz), 0.0));
 	
-	//Particle up vector = particle right x look
+	// Particle up vector = particle right x look
 	vec4 up = normalize(vec4(cross(look.xyz, right.xyz), 0.0));
 	
-	gl_Position = projectionMatrix*viewMatrix*(gl_in[0].gl_Position - right + up);
+	gl_Position = projectionMatrix * (viewMatrix * (gl_in[0].gl_Position - right + up));
 	vertexOut.tex_coords = vec2(0.0, 0.0);
 	vertexOut.alpha = vertexIn[0].alpha;
 	EmitVertex();
 
-	gl_Position = projectionMatrix*viewMatrix*(gl_in[0].gl_Position - right - up );
-	vertexOut.tex_coords = vec2(1.0,0.0);
+	gl_Position = projectionMatrix * (viewMatrix * (gl_in[0].gl_Position - right - up));
+	vertexOut.tex_coords = vec2(1.0, 0.0);
 	vertexOut.alpha = vertexIn[0].alpha;
 	EmitVertex();
 
-	gl_Position = projectionMatrix*viewMatrix*(gl_in[0].gl_Position + right + up );
+	gl_Position = projectionMatrix * (viewMatrix * (gl_in[0].gl_Position + right + up));
 	vertexOut.tex_coords = vec2(0.0, 1.0);
 	vertexOut.alpha = vertexIn[0].alpha;
 	EmitVertex();
 
-	gl_Position = projectionMatrix*viewMatrix*(gl_in[0].gl_Position + right - up );
+	gl_Position = projectionMatrix * (viewMatrix * (gl_in[0].gl_Position + right - up));
 	vertexOut.tex_coords = vec2(1.0, 1.0);
 	vertexOut.alpha = vertexIn[0].alpha;
 	EmitVertex();
